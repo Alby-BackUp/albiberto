b4dac42e1781b194a2c20100518326165b230fae
Alberto Viezzi <aviezzi@outlook.com>
2022-02-14 18:04:25 +0100

    Initial Commit

---
+++ Albiberto.Blazor/Program.cs
@@ -0,0 +1,32 @@
+using Albiberto.Blazor.Data;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+Enumerable.Range(0, 100).ToList().ForEach(i => logger.Information($"Hello, world {i}!"));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
