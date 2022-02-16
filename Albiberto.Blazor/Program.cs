eb50f2e4ee3e1db7921b07364ee5ae5b939df1f0
Alberto Viezzi <aviezzi@outlook.com>
2022-02-16 09:07:15 +0100

    Use ForwardedHeaders

--- Albiberto.Blazor/Program.cs
+++ Albiberto.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using Albiberto.Blazor.Data;
+using Microsoft.AspNetCore.HttpOverrides;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,12 +12,22 @@ Enumerable.Range(0, 100).ToList().ForEach(i =>
     logger.Information($"Hello, world {i}!")
 );
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
+
+// https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/proxy-load-balancer?view=aspnetcore-6.0#work-with-path-base-and-proxies-that-change-the-request-path
+
+app.UsePathBase("/app");
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
